f7a981f073991464511affab1bd3fb5d9475df69
jinsim <simsime@korea.ac.kr>
2022-02-27 15:21:25 +0900

    회원 도메인 실행과 테스트

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
+    // 인터페이스뿐만 아니라 구현체까지 의존하고 있음. DIP 위반
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
