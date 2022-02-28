098ed66e0c0c514beebc0c5dd963010281279211
jinsim <simsime@korea.ac.kr>
2022-02-28 16:03:34 +0900

    새로운 할인 정책 개발

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -4,9 +4,6 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
-import hello.core.order.Order;
-import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -25,6 +22,5 @@ public class OrderServiceTest {
 
         Order order = orderService.createOrder(memberId, "itemA", 10000);
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
-
     }
 }
