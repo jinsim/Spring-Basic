82d748d34597bb935c5d15adc97bb3112fe7b743
jinsim <simsime@korea.ac.kr>
2022-03-03 14:33:42 +0900

    싱글톤 방식의 주의점

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,7 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -47,7 +48,8 @@ public class SingletonTest {
     @DisplayName("스프링 컨테이너와 싱글톤")
     void springContainer() {
 
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        // AnnotationConfigApplicationContext이 길어서 ApplicationContext 으로 가져옴.
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
         MemberService memberService1 = ac.getBean("memberService", MemberService.class);
         MemberService memberService2 = ac.getBean("memberService", MemberService.class);
