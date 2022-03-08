7410f21262415635a3a1429d838f7d2b5afc86ec
jinsim <simsime@korea.ac.kr>
2022-03-08 14:49:48 +0900

    롬복과 최신 트렌드

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,11 +5,14 @@ import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 // 캄포넌트 등록을 위해
 @Component
+// final이 적용된 필드를 파라미터로 갖는 생성자를 만들어준다.
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     // DIP 위반
@@ -35,10 +38,13 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+//    @Autowired  // 생성자가 하나면 생략 가능하다.
+/*  // @RequiredArgsConstructor가 만들어준다.
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+*/
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
